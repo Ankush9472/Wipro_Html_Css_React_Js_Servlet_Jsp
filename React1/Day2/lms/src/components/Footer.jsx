dceef13816f98ae09e13783bc23e73d1c3ffddaf
Ankush9472 <ankushkumarccc@gmail.com>
2026-05-26 10:10:39 +0530

    Remove React1 folder

--- React1/Day2/lms/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="mt-20 bg-gray-800 text-white py-8 text-center">
-      <p>Your LMS Learning Portal 2026 Batches</p>
-    </footer>
-  );
-}
